e37b2bef61312bc707bcd91499500d9ccb31eb9e
Edgars Svarups <miclex82@tutanota.com>
2022-09-26 18:41:38 +0300

    Exercise #1 done

--- php-basics/flow-of-control/largest-number.php
+++ php-basics/flow-of-control/largest-number.php
@@ -7,3 +7,23 @@ echo "Input the 2nd number: ";
 echo "Input the 3rd number: ";
 
 //todo print the largest number
+
+?>
+
+<form action="largest-number.php" method="post">
+    1: <input type="number" name="first"><br>
+    2: <input type="number" name="second"><br>
+    3: <input type="number" name="third"><br>
+    <input type="submit">
+</form>
+
+<?php
+$array = [];
+if (isset($_POST["first"]) && isset($_POST["second"]) && isset($_POST["third"]))
+{
+    foreach($_POST as $key =>$value){
+        $array[] = $value;
+    }
+
+    print_r(max($array));
+}
